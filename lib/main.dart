64b56683b68fc677018d33be3e47b0e4e88be3f1
Bileks <bilisakumera@gmail.com>
2024-09-07 17:36:47 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:task/provider/task_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:task/utils/app_router.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
-void main() {
+Future<void> main() async {
     WidgetsFlutterBinding.ensureInitialized();
 tz.initializeTimeZones();
+
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  const initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+  const initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   runApp(
     ChangeNotifierProvider(
       create: (_) => TaskProvider(),
